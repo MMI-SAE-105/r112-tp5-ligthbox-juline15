c38b48da90d6589e8e8fde77a83f5593d63aa7f3
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2024-12-04 10:58:22 +0100

    ajout du script pour le click

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 // *** LIGHTBOX ***
 const lightbox = document.querySelector("#lightbox");
 const lightboxImg = document.querySelector("#lightbox > img");
+
+document.body.addEventListener("click", (evt) => {
+    if (evt.target.matches("[data-full-img]")) {
+        lightboxImg.src = evt.target.dataset.fullImg;
+        lightbox.showModal();
+    }
+});

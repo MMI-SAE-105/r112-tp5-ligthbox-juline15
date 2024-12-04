75a11ba3b46b7629914f705271184b50dc1866e2
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2024-12-04 11:16:27 +0100

    modification pour ajouter les animations

--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,17 @@ const lightboxImg = document.querySelector("#lightbox > img");
 document.body.addEventListener("click", (evt) => {
     if (evt.target.matches("[data-full-img]")) {
         lightboxImg.src = evt.target.dataset.fullImg;
+        lightbox.classList.add("entrée");
         lightbox.showModal();
     }
 });
 
 lightbox.addEventListener("click", (evt) => {
-    lightbox.classList.add("sortie");
-    lightbox.addEventListener("animationend", () => {
-        lightbox.classList.remove("sortie");
-        lightbox.close();
-    }, { once: true });
+    if (evt.target === lightbox) {
+        lightbox.classList.add("sortie");
+        lightbox.addEventListener("animationend", () => {
+            lightbox.classList.remove("sortie", "entrée");
+            lightbox.close();
+        }, { once: true });
+    }
 });
